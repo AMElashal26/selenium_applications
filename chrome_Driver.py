577e55e2c5b8b4a488adba6afce317dda15aa666
AMElashal26 <35622292+AMElashal26@users.noreply.github.com>
2024-01-29 21:31:11 -0500

    Add files via upload
    Added feature to find specific link with text, and feature to click on that link

--- chrome_Driver.py
+++ chrome_Driver.py
@@ -1,13 +1,29 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+
 options = Options()
 options.add_experimental_option("detach", True)
-driver = webdriver.Chrome(options= options)
+
+driver = webdriver.Chrome(service = Service(ChromeDriverManager().install()),
+                          options=options)
+
 
 #driver = webdriver.Chrome()
-driver.get("https://techwithtim.net")
-print(driver.title)
-driver.close()
+driver.get("https://neuralnine.com")
+driver.maximize_window()
+#print(driver.title)
+
+links = driver.find_elements('xpath','//a[@href]') # // = in whole page, find all anchor tags that has attribute href
+for link in links:
+    if  "Books" in link.get_attribute("innerHTML"):
+        link.click()
+        break
+    print(link.get_attribute("innerHTML"))
+
+
+#driver.close()
 
 '''1) you do not need to download the web driver anymore. Just use 
         
